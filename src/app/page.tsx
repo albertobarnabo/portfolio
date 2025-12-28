680c2c8d6e707a20c6474817e97532df99caa19b
instockr <instockr.business@gmail.com>
2025-12-28 18:27:34 +0100

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import BookOutlineIcon from "@/icons/BookOutlineIcon";
 import BookSolidIcon from "@/icons/BookSolidIcon";
 import { useState } from "react";
