ec58c0137fce5b958e2e71cab223fe52d1ec7499
instockr <instockr.business@gmail.com>
2026-02-22 23:20:01 +0100

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import AboutMe from "../components/AboutMe";
 import Tools from "../components/Tools";
 import Contact from "../components/Contacts";
